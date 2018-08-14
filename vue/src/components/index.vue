d24c25ba56cb39ee90ed4d5657e4a60172e12808
RicardoLu <ricardelu@outlook.com>
2018-08-14 16:24:20 +0800

    finish

--- vue/src/components/index.vue
+++ vue/src/components/index.vue
@@ -13,36 +13,64 @@
 
 <script>
 export default {
-    computed:{
-        items:function(){
-            // let promise = fetch('http://ccssbb.cn/',{
-            //     method:'get'
-            // })
-            // return promise.then(function(response){
-            //     return response.json()
-            // })
-            return [
-                {
-                    'article':'article',
-                    'article_title':'article_title',
-                    'id':'1',
-                    'time':'1'
-                },
-                {
-                    'article':'article',
-                    'article_title':'article_title',
-                    'id':'2',
-                    'time':'2'
-                },
-                {
-                    'article':'article',
-                    'article_title':'article_title',
-                    'id':'3',
-                    'time':'3'
-                }
-            ]
+    beforeCreate(){ 
+            
+    },
+    data(){
+        let promise = fetch('http://ccssbb.cn/',{
+                method:'get'
+            })
+            window.items = []
+            promise.then(Response => {console.log('2') ;return Response.json().then(json=>{
+                 for (var i in json){
+                     items.push(json[i])
+                 }
+                 console.log(window.items)
+                 return json
+                 })})
+        return {
+            items:window.items
         }
-    }
+    },
+    // methods:{
+    //     get_items(){
+    //          let promise = fetch('http://ccssbb.cn/',{
+    //             method:'get'
+    //         })
+    //     var items = []
+    //     return promise.then(function(response){
+    //             return response.json().then(function(json){
+    //                 console.log(json)
+    //                 return json
+    //             })
+    //         })
+    //     }
+    // }
+    // computed:{
+    //     items:function(){}
+            
+            // return [
+            //     {
+            //         'article':'article',
+            //         'article_title':'article_title',
+            //         'id':'1',
+            //         'time':'1'
+            //     },
+            //     {
+            //         'article':'article',
+            //         'article_title':'article_title',
+            //         'id':'2',
+            //         'time':'2'
+            //     },
+            //     {
+            //         'article':'article',
+            //         'article_title':'article_title',
+            //         'id':'3',
+            //         'time':'3'
+            //     }
+            // ]
+    //     }
+    // }
 }
 </script>
 
